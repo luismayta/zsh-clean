2333da5d5bebcdccc6782a06c3d4e6e855940c88
@slovacus <slovacus@gmail.com>
2019-10-01 11:38:17 -0500

    (#5) chore: upgrade list path by python gitignore

--- zsh-clean.zsh
+++ zsh-clean.zsh
@@ -14,28 +14,34 @@ CLEAR='\033[0m'
 function cleanup::all {
     echo -e "${CLEAR}${LIGHT_GREEN}Clean files${CLEAR}"
     find . -name '__pycache__' -delete -print -o \
-         -name '.DS_Store' -delete -print -o \
-         -name '*.pyc' -delete -print -o \
-         -name '*.orig' -delete -print -o \
-         -name '*.retry' -delete -print -o \
-         -name '*.tmp' -delete -print -o \
-         -name 'node_modules' -type d -print -exec rm -rf {} + -o \
-         -name 'jspm_packages' -type d -print -exec rm -rf {} + -o \
-         -name 'typings' -type d -print -exec rm -rf {} + -o \
-         -name '.npm' -type d -print -exec rm -rf {} + -o \
-         -name '.vagrant' -type d -print -exec rm -rf {} + -o \
-         -name '.wercker' -type d -print -exec rm -rf {} + -o \
-         -name '.eggs' -type d -print -exec rm -rf {} + -o \
-         -name 'eggs' -type d -print -exec rm -rf {} + -o \
-         -name 'htmlcov' -type d -print -exec rm -rf {} + -o \
-         -name '.mypy_cache' -type d -print -exec rm -rf {} + -o \
-         -name '.hypothesis' -type d -print -exec rm -rf {} + -o \
-         -name '.lib-cov' -type d -print -exec rm -rf {} + -o \
-         -name 'bower_components' -type d -print -exec rm -rf {} + -o \
-         -name '__pycache__' -type d -print -exec rm -rf {} + -o \
-         -name '.venv' -type d -print -exec rm -rf {} + -o \
-         -name 'env.back' -type d -print -exec rm -rf {} + -o \
-         -name 'venv.back' -type d -print -exec rm -rf {} + -o \
-         -name '.terraform' -type d -print -exec rm -rf {} + -o \
-         -name 'coverage' -type d -print -exec rm -rf {} +
+        -name '.DS_Store' -delete -print -o \
+        -name '*.pyc' -delete -print -o \
+        -name '*.orig' -delete -print -o \
+        -name '*.retry' -delete -print -o \
+        -name '*.tmp' -delete -print -o \
+        -name '*.egg' -delete -print -o \
+        -name 'node_modules' -type d -print -exec rm -rf {} + -o \
+        -name 'jspm_packages' -type d -print -exec rm -rf {} + -o \
+        -name 'typings' -type d -print -exec rm -rf {} + -o \
+        -name '.npm' -type d -print -exec rm -rf {} + -o \
+        -name '.vagrant' -type d -print -exec rm -rf {} + -o \
+        -name '.wercker' -type d -print -exec rm -rf {} + -o \
+        -name '.eggs' -type d -print -exec rm -rf {} + -o \
+        -name 'eggs' -type d -print -exec rm -rf {} + -o \
+        -name '*.egg-info' -type d -print -exec rm -rf {} + -o \
+        -name '.pytest_cache' -type d -print -exec rm -rf {} + -o \
+        -name '.hypothesis' -type d -print -exec rm -rf {} + -o \
+        -name 'docs/_build/' -type d -print -exec rm -rf {} + -o \
+        -name 'htmlcov' -type d -print -exec rm -rf {} + -o \
+        -name '.mypy_cache' -type d -print -exec rm -rf {} + -o \
+        -name '.hypothesis' -type d -print -exec rm -rf {} + -o \
+        -name 'wheels' -type d -print -exec rm -rf {} + -o \
+        -name '.lib-cov' -type d -print -exec rm -rf {} + -o \
+        -name 'bower_components' -type d -print -exec rm -rf {} + -o \
+        -name '__pycache__' -type d -print -exec rm -rf {} + -o \
+        -name '.venv' -type d -print -exec rm -rf {} + -o \
+        -name 'env.back' -type d -print -exec rm -rf {} + -o \
+        -name 'venv.back' -type d -print -exec rm -rf {} + -o \
+        -name '.terraform' -type d -print -exec rm -rf {} + -o \
+        -name 'coverage' -type d -print -exec rm -rf {} +
 }
